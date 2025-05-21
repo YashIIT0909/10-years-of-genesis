96cbd8c4228ce9cea3bc44541c99a0918d44ad15
Ranish-Garg <ranishgarg2006@gmail.com>
2025-05-22 00:42:50 +0530

    aboutpage

---
+++ src/pages/AboutusPage/card.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Card.css'; // Import external CSS
+
+const Card = (props) => {
+
+
+    console.log(props.achievement)
+
+
+  return (
+    <div className="card-wrapper">
+      <div 
+      className="card" 
+     style={{ backgroundImage: `url(${props.achievement.image})` }}>
+       
+        <div className="card__content">
+        <h3 className="card__title">{props.achievement.title}</h3>
+        <p className="card__subtitle">
+          <strong>Conducted By:</strong> {props.achievement.conductedby} 
+        </p>
+        <p  className="card__subtitle">
+             <strong>Mode:</strong> {props.achievement.Mode}
+        </p>
+        <p className="card__winner">
+         <strong>Winner:</strong> {props.achievement.winner}
+        </p>
+        <p className="card__description">{props.achievement.description}</p>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
